6c808f12ecbb20a39f0893484dd5837238584355
vardhan12178 <balavardhan12178@gmail.com>
2024-07-16 11:36:15 +0530

    Updated header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,7 +3,7 @@ import { Link, NavLink } from 'react-router-dom';
 import { SearchContext } from '../SearchContext';
 
 const Header = () => {
-  const [hidden, setHidden] = useState(true);
+  const [menuOpen, setMenuOpen] = useState(false);
   const [darkMode, setDarkMode] = useState(JSON.parse(localStorage.getItem('darkMode')) || false);
   const { setSearchTerm } = useContext(SearchContext);
 
@@ -36,15 +36,12 @@ const Header = () => {
           <div id="mobile-nav" className="flex md:order-2">
             <button
               onClick={() => setDarkMode(!darkMode)}
-              data-tooltip-target="navbar-search-example-toggle-dark-mode-tooltip"
               type="button"
-              data-toggle-dark="light"
               className="flex items-center p-2 mr-2 text-xs font-medium text-gray-700 bg-white rounded-lg border border-gray-200 toggle-dark-state-example hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-gray-300 dark:focus:ring-gray-500 dark:bg-gray-800 focus:outline-none dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
             >
               {darkMode ? (
                 <svg
                   aria-hidden="true"
-                  data-toggle-icon="sun"
                   className="w-4 h-4"
                   fill="currentColor"
                   viewBox="0 0 20 20"
@@ -59,7 +56,6 @@ const Header = () => {
               ) : (
                 <svg
                   aria-hidden="true"
-                  data-toggle-icon="moon"
                   className="w-4 h-4"
                   fill="currentColor"
                   viewBox="0 0 20 20"
@@ -70,11 +66,8 @@ const Header = () => {
               )}
             </button>
             <button
-              onClick={() => setHidden(!hidden)}
+              onClick={() => setMenuOpen(!menuOpen)}
               type="button"
-              data-collapse-toggle="navbar-search"
-              aria-controls="navbar-search"
-              aria-expanded="false"
               className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1"
             >
               <svg
@@ -86,40 +79,15 @@ const Header = () => {
               >
                 <path
                   fillRule="evenodd"
-                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
                   clipRule="evenodd"
                 ></path>
               </svg>
-              <span className="sr-only">Search</span>
+              <span className="sr-only">Open main menu</span>
             </button>
-            <div className="hidden relative md:block">
-              <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-                <svg
-                  className="w-5 h-5 text-gray-500"
-                  aria-hidden="true"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
-                <span className="sr-only">Search icon</span>
-              </div>
-              <input
-                type="text"
-                id="search-navbar"
-                onChange={handleSearchChange}
-                className="block p-2 pl-10 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="Search..."
-              />
-            </div>
           </div>
 
-          <div className="hidden justify-between items-center w-full md:flex md:w-auto md:order-1" id="navbar-search">
+          <div className={`w-full md:flex md:w-auto md:order-1 ${menuOpen ? 'block' : 'hidden'}`} id="navbar-search">
             <div className="relative mt-3 md:hidden">
               <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
                 <svg
